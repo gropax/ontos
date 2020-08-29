d5af08889ea835e2caaed9792b54e067cf15f669
Maxime Laudrin <maxime.laudrin@protonmail.com>
2020-08-29 17:43:57 +0200

    Successfuly interact with Neo4J !!!

---
+++ Ontos.Storage.Tests/GraphStorageTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Ontos.Storage.Tests
+{
+    public class GraphStorageTests
+    {
+        private GraphStorage _storage;
+
+        public GraphStorageTests()
+        {
+            _storage = new GraphStorage("bolt://localhost:7687/db/data", "neo4j", "1234");
+        }
+
+        [Fact]
+        public async void TestCreateContent()
+        {
+            var newContent = new NewContent("Contenu super cool.");
+
+            var content = await _storage.CreateContent(newContent);
+
+            Assert.True(content.Id > 0);
+            Assert.Equal("Contenu super cool.", content.Details);
+        }
+    }
+}
---
+++ Ontos.Storage/GraphStorage.cs
@@ -0,0 +1,97 @@
+﻿using Neo4j.Driver;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ontos.Storage
+{
+    public interface IGraphStorage
+    {
+    }
+
+    public class GraphStorage : IGraphStorage
+    {
+        private IDriver _driver;
+
+        public GraphStorage(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public async Task<Content> CreateContent(NewContent newContent)
+        {
+            var session = _driver.AsyncSession();
+
+            try
+            {
+                var cursor = await session.RunAsync(@"
+                    CREATE (c:Content $content)
+                    RETURN c",
+                    new { content = new { details = newContent.Description } });
+
+                var content = await cursor.ToListAsync(r =>
+                {
+                    var node = r["c"].As<INode>();
+                    return new Content()
+                    {
+                        Id = node.Id,
+                        Details = node["details"].As<string>()
+                    };
+                });
+
+                return content.First();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+    }
+
+    public class NewContent
+    {
+        public string Description { get; }
+        public NewContent(string content)
+        {
+            Description = content;
+        }
+    }
+
+    public class Content
+    {
+        public long Id { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class Relation
+    {
+        public int Id { get; set; }
+        public string Details { get; set; }
+    }
+
+    /// <summary>
+    /// In relation with :
+    ///   - Content through a Reference relation
+    ///   - Expression through a Translation relation
+    /// </summary>
+    public class Expression
+    {
+        public string Language { get; set; }
+        public string Label { get; set; }
+    }
+
+    /// <summary>
+    /// Represent a denotation relation between an Expression and a Content, in zero or many specific Contexts
+    /// </summary>
+    public class Reference
+    {
+        public string[] Contexts { get; set; }
+    }
+}
